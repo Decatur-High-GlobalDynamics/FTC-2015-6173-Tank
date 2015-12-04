16871688b4a87119f3e29d1418e2926bd3fbf526
kcraig <ken@craigs.us>
2015-12-04 16:23:07 -0500

    * Changed the name of the application in the strings.xml file to "6173 Robot Controller" * Reset the arm positions when stopped * Only change the position of the servo when actually moved with the controls (may be a bad idea, untested)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArcadeTeleOp6173.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArcadeTeleOp6173.java
@@ -26,8 +26,8 @@ public class ArcadeTeleOp6173 extends OpMode {
     double leftArmPosition;
     double rightArmPosition;
 
-    double leftArmDelay = 0;
-    double rightArmDelay = 0;
+    double leftArmWaitUntil = 0;
+    double rightArmWaitUntil = 0;
 
     // amount to change the arm servo position.
     double armDelta = 0.1;
@@ -67,13 +67,6 @@ public class ArcadeTeleOp6173 extends OpMode {
 		 * that the names of the devices must match the names used when you
 		 * configured your robot and created the configuration file.
 		 */
-		
-		/*
-		 *   
-		 * We also assume that there are two servos "servo_1" and "servo_6"
-		 *    "servo_1" controls the arm joint of the manipulator.
-		 *    "servo_6" controls the claw joint of the manipulator.
-		 */
 
         driveLeft = hardwareMap.dcMotor.get("left_front");
         driveLeft2 = hardwareMap.dcMotor.get("left_back");
@@ -90,25 +83,14 @@ public class ArcadeTeleOp6173 extends OpMode {
         servoRight = hardwareMap.servo.get("servo_right");
         servoLeft.setDirection(Servo.Direction.FORWARD);
 
+        resetArmPositions();
+    }
+
+    private void resetArmPositions() {
         leftArmPosition = LEFT_ARM_MIN; //(LEFT_ARM_MAX-LEFT_ARM_MIN)/2;
         rightArmPosition = RIGHT_ARM_MAX; //(RIGHT_ARM_MAX-RIGHT_ARM_MIN)/2;
-
-        /*
-        powerStep = 0.01f;
-        currentLeftPower = 0.0f;
-        currentRightPower = 0.0f;
-        lastGamePad1Y = false;
-        */
-
-
-        //servoRightFront = hardwareMap.servo.get("right_front_servo");
-        //servoRightRear = hardwareMap.servo.get("right_rear_servo");
-        //servoLeftFront = hardwareMap.servo.get("left_front_servo");
-        //servoLeftRear = hardwareMap.servo.get("left_rear_servo");
-
-        //servoLeftFront.setPosition(.5);
-        //servoLeftRear.setPosition(.5);
-        //servoRightRear.setPosition(.5);
+        servoLeft.setPosition(leftArmPosition);
+        servoRight.setPosition(rightArmPosition);
     }
 
     /*
@@ -120,75 +102,65 @@ public class ArcadeTeleOp6173 extends OpMode {
     public void loop() {
 
 
-        float leftThrottle = gamepad1.left_stick_y;
-        float rightThrottle = gamepad1.right_stick_y;
+        double leftThrottle = gamepad1.left_stick_y;
+        double rightThrottle = gamepad1.right_stick_y;
 
 
         // clip the right/left values so that the values never exceed +/- 1
         leftThrottle = Range.clip(leftThrottle, -1, 1);
         rightThrottle = Range.clip(rightThrottle, -1, 1);
 
+        //scale the throttle so it's easier to control at low speeds
+        //this is UNTESTED on the actual robot as of 12/4/2015, so just comment out if it's acting up
+        leftThrottle = scaleInput(leftThrottle);
+        rightThrottle = scaleInput(rightThrottle);
+
         driveRight.setPower(rightThrottle);
         driveRight2.setPower(rightThrottle);
         driveLeft.setPower(leftThrottle);
         driveLeft2.setPower(leftThrottle);
-        // scale the joystick value to make it easier to control
-        // the robot more precisely at slower speeds.
 
+        boolean moveLeftArm = false;
+        boolean moveRightArm = false;
 
-        // clip the right/left values so that the values never exceed +/- 1
-
-        // update the position of the arm.
-//    if (gamepad1.a) {
-//      // if the A button is pushed on gamepad1, increment the position of
-//      // the arm servo.
-//      // servoRightFront.setPosition(0.5);
-//    } else {
-//      //servoRightFront.setPosition(0.2);
-//    }
-//    if (gamepad1.right_bumper == true) {
-//      servoRight.setPosition(.5);
-//    } else if (gamepad1.left_bumper == true) {
-//      servoLeft.setPosition(.5);
-//    } else {
-//      servoLeft.setPosition(1);
-//      servoRight.setPosition(1);
-//
-//    }
-
-        if (getRuntime() >= leftArmDelay) {
+        if (getRuntime() >= leftArmWaitUntil) { //pause before changing position again
             if (gamepad1.left_bumper) {
                 leftArmPosition -= armDelta;
-                leftArmDelay = getRuntime() + ARM_DELAY;
+                moveLeftArm = true;
             }
 
             if (gamepad1.left_trigger > 0.25) {
                 leftArmPosition += armDelta;
-                leftArmDelay = getRuntime() + ARM_DELAY;
+                moveLeftArm = true;
+            }
+
+            if (moveLeftArm) {
+                leftArmPosition = Range.clip(leftArmPosition, LEFT_ARM_MIN, LEFT_ARM_MAX);
+                servoLeft.setPosition(leftArmPosition);
+                leftArmWaitUntil = getRuntime() + ARM_DELAY;
             }
         }
-        if (getRuntime() >= rightArmDelay) {
+
+        if (getRuntime() >= rightArmWaitUntil) {
             if (gamepad1.right_bumper) {
                 rightArmPosition += armDelta;
-                rightArmDelay = getRuntime() + ARM_DELAY;
+                moveRightArm = true;
             }
 
             if (gamepad1.right_trigger > 0.25) {
                 rightArmPosition -= armDelta;
-                rightArmDelay = getRuntime() + ARM_DELAY;
+                moveRightArm = true;
+            }
+
+            if (moveRightArm) {
+                rightArmPosition = Range.clip(rightArmPosition, RIGHT_ARM_MIN, RIGHT_ARM_MAX);
+                servoRight.setPosition(rightArmPosition);
+                rightArmWaitUntil = getRuntime() + ARM_DELAY;
             }
         }
 
-        // clip the position values so that they never exceed their allowed range.
-        leftArmPosition = Range.clip(leftArmPosition, LEFT_ARM_MIN, LEFT_ARM_MAX);
-        rightArmPosition = Range.clip(rightArmPosition, RIGHT_ARM_MIN, RIGHT_ARM_MAX);
-
-        // write position values to the wrist and claw servo
-        servoLeft.setPosition(leftArmPosition);
-        servoRight.setPosition(rightArmPosition);
-
 /*
-		 * Send telemetry data back to driver station. Note that if we are using
+         * Send telemetry data back to driver station. Note that if we are using
 		 * a legacy NXT-compatible motor controller, then the getPower() method
 		 * will return a null value. The legacy NXT-compatible motor controllers
 		 * are currently write only.
@@ -209,17 +181,42 @@ public class ArcadeTeleOp6173 extends OpMode {
      */
     @Override
     public void stop() {
-
+        resetArmPositions();
     }
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal) {
+        double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
 }
-
-  /*
-    * This method scales the joystick input so for low joystick values, the
-    * scaled value is less than linear.  This is to make it easier to drive
-    * the robot more precisely at slower speeds.
-*/
-
-
-// index cannot exceed size of array minus 1.
-
 
